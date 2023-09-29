6621f56a963332b652beafcbd358a57ef7b54770
vianita02 <est18500@byui.edu>
2023-09-29 11:53:55 -0500

    changes

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,5 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  // console.log('asjasiiasj');
-
-  let currentCart = getLocalStorage("so-cart") || [];
-
-  currentCart.push(product);
-
-  setLocalStorage("so-cart", currentCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-const addCartButton = document.querySelector("#addToCart");
-// add listener to Add to Cart button
-addCartButton.addEventListener("click", addToCartHandler);
+const productId = getParam("product");
+productDetails(productId);
